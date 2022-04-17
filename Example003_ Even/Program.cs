81ee2642a40b038016bf8c56646a29755975ecb5
Eugeny <76texac@gmail.com>
2022-04-18 00:08:14 +0300

    Example003 Even?

---
+++ Example003_ Even/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+if (a%2==0)
+{
+      Console.WriteLine("Да");
+}
+
+else
+{
+      Console.WriteLine("Нет");
+}
