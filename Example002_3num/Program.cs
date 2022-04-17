888c9218bbaf70b8d85a07991059493e27ee879c
Eugeny <76texac@gmail.com>
2022-04-18 00:01:36 +0300

    Example002 max of 3num

---
+++ Example002_3num/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+if (num1>num2 & num1>num3)
+ {
+    Console.WriteLine($"Max=  {num1} " );
+ }
+
+ else if (num2>num1 & num2>num3)
+ {
+    Console.WriteLine($"Max=  {num2} " );
+ }
+
+ else 
+ {
+       Console.WriteLine($"Max=  {num3} " );
+ }
+ 
